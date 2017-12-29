4821f72aa653b22171d4c381825ea77ea76f287d
randomness38 <randomness.jay@gmail.com>
2017-12-29 10:17:30 +0900

    Update query () method doesn't work!
    슈도코드는 논리적이라고 생각했는데
    안먹히네에?? 왜지왜지
    그럼 API 메소드를 써야 하는거지? 왜지왜지?

--- src/Components/SearchList/SearchList.js
+++ src/Components/SearchList/SearchList.js
@@ -1,41 +1,59 @@
 import React , { Component } from 'react'
 import { Link } from 'react-router-dom'
 import BookList from "../BookList/BookList";
+import escapeRegExp from 'escape-string-regexp'
 
 class SearchList extends Component {
     constructor(props){
         super(props);
         this.state = {
             query: '',
+            displayBooks:[],
         }
-        this.handleTermChange = this.handleTermChange.bind(this);
-        this.handleKeyPress = this.handleKeyPress.bind(this);
+        // this.handleTermChange = this.handleTermChange.bind(this);
+        this.updateQuery = this.updateQuery.bind(this);
+        // this.handleKeyPress = this.handleKeyPress.bind(this);
         this.search = this.search.bind(this);
     }
 
+    updateQuery = (query) => {
+        this.setState({query : query.trim() });
 
+        let showingBooks
+        if (this.state.query){
+            const match = new RegExp(escapeRegExp(this.state.query),'i');
+            showingBooks = this.props.searchList.filter((book) =>
+                match.test(book.title))
+        } else {
+            showingBooks = this.props.searchList
+        }
+
+        this.setState({displayBooks: showingBooks})
+
+    }
 
     search() {
         this.props.onSearch(this.state.query)
 
     }
 
+    //
+    // handleTermChange(event) {
+    //     this.setState({
+    //         query: event.target.value
+    //     });
+    // }
 
-    handleTermChange(event) {
-        this.setState({
-            query: event.target.value
-        });
-    }
-
-
-    handleKeyPress(event) {
-        if (event.key === 'Enter') this.search();
-        }
+    //
+    // handleKeyPress(event) {
+    //     if (event.key === 'Enter') this.search();
+    //     }
 
 
     render() {
         return(
             <div>
+
             <div className="search-books">
                 <div className="search-books-bar">
                     <Link to="/" className="close-search" >Close</Link>
@@ -43,13 +61,17 @@ class SearchList extends Component {
                         <input id='SearchBar'
                                type="text"
                                placeholder="Search by title or author"
-                               onChange={this.handleTermChange}
-                               onKeyPress={this.handleKeyPress}
-                               defaultValue={this.props.searchTerm} />
+                               // onKeyPress={this.handleKeyPress}
+                               value={this.state.query}
+                               onChange={(event) => this.updateQuery(event.target.value)}
+                               // defaultValue={this.props.searchTerm}
+                        />
+                        {JSON.stringify(this.state)}
                     </div>
                 </div>
+
                 <div className="search-books-results">
-                    <BookList books={this.props.searchList}
+                    <BookList books={this.state.displayBooks}
                               onUpdate={this.props.onUpdate} />
                 </div>
             </div>
